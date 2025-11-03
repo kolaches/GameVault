936e755a5ed5fb68097538bc27230b036a487321
kolaches <djudosoej247hdh@outlook.com>
2025-11-03 20:21:44 +0200

    Add project files

--- GameVault/vite.config.js
+++ GameVault/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/calculTOR/', // имя репозитория на GitHub
   plugins: [react()],
-  base: '/GameVault/'
 })
