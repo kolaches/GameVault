96cd51f88d09a2e9daa9aef15f3c65c052062aa6
kolaches <djudosoej247hdh@outlook.com>
2025-11-03 20:30:38 +0200

    Fix base path for GitHub Pages

--- GameVault/vite.config.js
+++ GameVault/vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/calculTOR/', // имя репозитория на GitHub
-  plugins: [react()],
-})
+  base: "/GameVault/",
+});
